6a99efa104bb4218f16015b023c762b3ba8f17b3
John Melody Me <Johnmelodyme@yandex.com>
2022-01-29 04:13:31 +0800

    Added auth Page (WPF)

---
+++ Monitor-PC/EmergenciesDemoMonitor/Application.xaml.cs
@@ -0,0 +1,86 @@
+﻿using EmergenciesDemoMonitor.utilities;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace EmergenciesDemoMonitor
+{
+    public partial class Application : Window
+    {
+        public const int STATUS_CODE = 0xC8;
+        public string UserUrl = Constants.userEndpoint;
+        private static readonly HttpClient client = new HttpClient();
+
+        public Application()
+        {
+            InitializeComponent();
+
+            this.UserComponents();
+        }
+
+        public void UserComponents()
+        {
+            this.login.FontSize = 0xB;
+            this.login.Content = "LOGIN";
+        }
+
+
+        public static bool IsEmailValid(String email)
+        {
+            if (!(Regex.IsMatch(
+                email,
+                @"^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$"
+            )))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private async Task GetUser(string Url)
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            Task<string> stringTask = client.GetStringAsync(Url);
+            
+            await stringTask;
+        
+            Utilities.log(Message: stringTask.Result, IsDebug: true);
+
+        }
+
+        private async void Login(object sender, RoutedEventArgs routedEventArgs)
+        {
+            Utilities.log(Message: "...login", IsDebug: false);
+           
+            if (this.loginEmail.Text.Length == 0x0)
+            {
+                this.errormessage.Text = "Your email address is required";
+                this.loginEmail.Focus();
+
+                Utilities.log(Message:"...email is empty", IsDebug: true);
+            }
+            else
+            {
+                if (!(IsEmailValid(this.loginEmail.Text)))
+                {
+                    this.errormessage.Text = "Your Email is invalid";
+                    this.loginEmail.Select(start: 0x0, length: this.loginEmail.Text.Length);
+                    this.loginEmail.Focus();
+
+                    Utilities.log(Message:"...email is invalid", IsDebug: true);
+                }
+
+                await this.GetUser(Url: this.UserUrl);
+            }
+        }
+    }
+}
---
+++ Monitor-PC/EmergenciesDemoMonitor/utilities/Utilities.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using static EmergenciesDemoMonitor.Constants;
+
+namespace EmergenciesDemoMonitor.utilities
+{
+    public class Utilities
+    {
+        public Utilities() {
+        }
+
+        public static void log(string Message, bool IsDebug)
+        {
+            if (IsDebug)
+            {
+                Debug.WriteLine("[debug]: " + Message);
+            }
+            else
+            {
+                Debug.WriteLine("[info]: " + Message);
+            }
+        }
+    }
+}
